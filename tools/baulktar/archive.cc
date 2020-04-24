a43dd000eadd5e3d48fe7a19932431b73a8f2495
Force.Charlie <charlieio@outlook.com>
2020-04-24 22:53:44 +0800

    bsdtar code

---
+++ tools/baulktar/archive.cc
@@ -0,0 +1,10 @@
+///
+#include <string_view>
+#include <archive.h>
+
+namespace baulktar {
+void openfile(std::wstring_view file) {
+  // archive *a=nullptr;
+  // archive_read_disk_open_w(&a,)
+}
+} // namespace baulktar
