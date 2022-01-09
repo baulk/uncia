2229b7ea67cc5b2409d7d1b8abc0afc395551980
Force Charlie <charlieio@outlook.com>
2022-01-09 22:39:28 +0800

    uncia: 1.99.0

--- tools/uncia/uncia.cc
+++ tools/uncia/uncia.cc
@@ -6,18 +6,8 @@
 
 namespace uncia {
 bool IsDebugMode = false;
-
 void Version() { bela::FPrintF(stderr, L"uncia %s %s\n", UNCIA_VERSION, archive_version_details()); }
-// -x --extract
-// -o --output
-// -h --help
-// -v --version
-// -V --verbose
-// -c --create
-// -f --format
-// -z, -j, -J, --lzma --zstd
-// -l --list
-// --exclude
+
 } // namespace uncia
 
 int wmain(int argc, wchar_t **argv) {
